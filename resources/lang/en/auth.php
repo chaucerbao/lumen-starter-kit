eff6949d747e444a2bdc394729db1a567a736073
Alvin Chan <aschan@gmail.com>
2015-05-11 17:00:54 -0700

    Sort the Auth/User language translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -2,8 +2,8 @@
 
 return [
     'bad_token' => 'Invalid token.',
-    'registration_confirmation' => 'You have successfully registered a new account. Please check your e-mail for a confirmation.',
-    'reset_requested' => 'Instructions to reset your password have been sent to your e-mail address.',
     'email_confirmed' => 'You have successfully confirmed your e-mail address.',
     'password_reset' => 'Your password has been successfully set.',
+    'registration_confirmation' => 'You have successfully registered a new account. Please check your e-mail for a confirmation.',
+    'reset_requested' => 'Instructions to reset your password have been sent to your e-mail address.',
 ];
